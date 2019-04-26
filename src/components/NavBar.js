db90c4412318f336112007542c037205cf72b3b3
Farhan <farhanmahmoodny@gmail.com>
2019-04-26 13:43:37 -0400

    photocard fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,40 +4,40 @@ import { NavLink } from "react-router-dom";
 export default class NavBar extends React.Component {
   render() {
     return (
-      <div>
+      <div className='NavBar'>
         <ul>
           <li>
             <div>
-              <NavLink to='/'>Home</NavLink>
+              <NavLink className='button' to='/'>Home</NavLink>
             </div>
           </li>
           <li>
             <div>
-              <NavLink to='/memories'>Memories</NavLink>
+              <NavLink className='button' to='/memories'>Memories</NavLink>
             </div>
           </li>
           {this.props.activeUser === null ?
           (<div>
             <li>
             <div>
-              <NavLink to='/login'>Log-In</NavLink>
+              <NavLink className='button' to='/login'>Log-In</NavLink>
             </div>
           </li>
           <li>
             <div>
-              <NavLink to='/signup'>Sign Up</NavLink>
+              <NavLink className='button' to='/signup'>Sign Up</NavLink>
             </div>
           </li>
           </div>) :
           (<div>
             <li>
             <div>
-              <NavLink to='/profile'>Profile</NavLink>
+              <NavLink className='button' to='/profile'>Profile</NavLink>
             </div>
           </li>
           <li>
             <div>
-              <NavLink onClick={this.props.logout} to='/'>Log-Out</NavLink>
+              <NavLink className='button' onClick={this.props.logout} to='/'>Log-Out</NavLink>
             </div>
           </li>
           </div>)
--- src/components/photoCard.js
+++ src/components/photoCard.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 class PhotoCard extends React.Component {
+
+  state = {
+    id: this.props.memory.id,
+    location: this.props.memory.location,
+    image: this.props.memory.image,
+    description: this.props.memory.description
+  }
+
+  clickHandler = () => {
+    console.log('cleeeeked!', this.state)
+  }
+
   render() {
     return (
-      <div>
+      <div onClick={this.clickHandler}>
         <h5>{this.props.memory.location}</h5>
         <img src={this.props.memory.image} alt="not working"/>
         <p>{this.props.memory.description}</p>
