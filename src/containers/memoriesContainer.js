80b68ddd51f62fdeb7c9b3b3ddd1e133ba888cc4
Farhan <farhanmahmoodny@gmail.com>
2019-05-08 09:34:26 -0400

    fixed css

--- src/containers/memoriesContainer.js
+++ src/containers/memoriesContainer.js
@@ -46,8 +46,8 @@ class MemoriesContainer extends React.Component{
         <form className='search-form'>
           <input type='text' name='search' value={this.state.search} placeholder='Search' className='search-input' onChange={this.searchHandler}/>
         </form>
-        <div>
-          <select className='sort-by' onChange={this.props.sortHandler}>
+        <div className='sort-by'>
+          <select onChange={this.props.sortHandler}>
             <option>Sort By</option>
             <option>Alphabeitcal: A-Z</option>
             <option>Alphabetical: Z-A</option>
